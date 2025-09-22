cb97cf399eb8d61bc42a7e55ddfd68630bfa7705
wael <Wael.slama.issat@gmail.com>
2025-09-22 11:35:48 +0100

    feat: initialize NestJS application with basic structure and coffee module
    - Added package.json with dependencies and scripts for the project.
    - Created AppController and AppService with a simple "Hello World!" endpoint.
    - Implemented CoffeesModule with CoffeesController and CoffeesService for managing coffee data.
    - Added DTOs for creating and updating coffee entries.
    - Implemented unit tests for AppController, CoffeesController, and CoffeesService.
    - Set up main entry point with validation pipe for incoming requests.
    - Included end-to-end tests for the application.
    - Configured TypeScript settings and Jest for testing.

---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,42 @@
+/* eslint-disable prettier/prettier */
+import {  Injectable, NotFoundException } from '@nestjs/common';
+import { Coffee } from 'src/entities/coffee.entity';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+
+@Injectable()
+export class CoffeesService {
+    private coffees : Coffee [] = [{
+        id: 1,
+        name: 'Shipwreck Roast',
+        brand: 'Buddy Brew',
+        flavors: ['chocolate', 'vanilla']      },]
+    findAll()  {
+        return this.coffees;
+    }   
+    findOne(id: number)  {
+        
+        const coffee = this.coffees.find(item => item.id === +id);
+        if (!coffee) {
+            throw new NotFoundException(`Coffee #${id} not found`);
+        }   
+        return coffee;
+    }
+    create(coffee: Coffee) {
+        this.coffees.push(coffee);      
+    }
+  update(id: number, updateCoffeeDto: UpdateCoffeeDto) {
+        const existingCoffee = this.findOne(id);
+        if (existingCoffee) {
+            Object.assign(existingCoffee, updateCoffeeDto);
+        }
+        return existingCoffee;
+    }
+    remove(id: number) {
+        const coffeeIndex = this.coffees.findIndex(item => item.id === id); 
+        if (coffeeIndex >= 0) {
+            this.coffees.splice(coffeeIndex, 1);
+            return true;
+        }   
+        return false;
+    }
+}
