34ebbd95ca5b5769b8eca9cc19aa8c1b6099d0c5
wael <Wael.slama.issat@gmail.com>
2025-10-09 12:44:17 +0100

    add DatabaseModule for dynamic database connection and update imports in CoffeesModule and CoffeeRatingModule

--- src/coffee-rating/coffee-rating.module.ts
+++ src/coffee-rating/coffee-rating.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { CoffeeRatingService } from './coffee-rating.service';
 import { CoffeesModule } from 'src/coffees/coffees.module';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    DatabaseModule.register({
+      type: 'postgres',
+      host: 'localhost',
+      password: 'pass123', // <-- Use the same as in docker-compose.yml
+      username: 'postgres',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
+      port: 5433,
+    }),
+    CoffeesModule,
+  ],
   providers: [CoffeeRatingService],
 })
 export class CoffeeRatingModule {}
---
+++ src/database/database.module.ts
@@ -0,0 +1,18 @@
+import { DynamicModule, Module } from '@nestjs/common';
+import { DataSourceOptions, createConnection } from 'typeorm';
+
+@Module({})
+export class DatabaseModule {
+  static register(options: DataSourceOptions): DynamicModule {
+    return {
+      module: DatabaseModule,
+      providers: [
+        {
+          provide: 'CONNECTION',
+          useValue: createConnection(options),
+        },
+      ],
+      exports: ['CONNECTION'],
+    };
+  }
+}
