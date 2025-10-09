ade445f751f0c5d8745056602240705fb0ee38f8
wael <Wael.slama.issat@gmail.com>
2025-10-09 12:12:33 +0100

    refactor CoffeesModule to remove CoffeeBrandsFactory and simplify COFFEE_BRANDS provider

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,4 +1,4 @@
-import { Injectable, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -7,24 +7,12 @@ import { Flavor } from './entities/flavor.entity/flavor.entity';
 import { Event } from 'src/events/entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
 
-@Injectable()
-export class CoffeeBrandsFactory {
-  create() {
-    return ['buddy brew', 'nescafe'];
-  }
-}
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
-    CoffeeBrandsFactory,
-    {
-      provide: COFFEE_BRANDS,
-      useFactory: (brandsFactory: CoffeeBrandsFactory) =>
-        brandsFactory.create(),
-      inject: [CoffeeBrandsFactory],
-    },
+    { provide: COFFEE_BRANDS, useFactory: () => ['buddy brew', 'nescafe'] },
   ],
   exports: [CoffeesService],
 })
