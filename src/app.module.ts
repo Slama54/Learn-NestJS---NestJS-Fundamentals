a1331d2fae4e50a3e423e436b7930e4f3c870f0f
wael <Wael.slama.issat@gmail.com>
2025-10-13 12:15:10 +0100

    add @hapi/joi for environment variable validation in ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(5433),
+        DATABASE_USER: Joi.required(),
+        DATABASE_PASSWORD: Joi.required(),
+        DATABASE_NAME: Joi.required(),
+      }),
+    }),
+
     CoffeesModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
